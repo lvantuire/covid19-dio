aeada90969751ffe9a5af961022b1673ca178813
lvantuire <lvantuire@gmail.com>
2022-05-18 16:16:03 -0300

    final da aula 4

--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,11 +1,35 @@
 import React, { memo } from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useCallback } from 'react'
+import { Container } from '../../../node_modules/@material-ui/core/index'
+import Api from '../../api'
+import { ContainerStyled } from './style'
 
 function Main() {
 
+    const [data, setData] = useState({})
+    const [country, setCountry] = useState('brazil')
+
+    const getCovidData = useCallback((country)=> {
+        Api.getCountry(country)
+            .then(data => setData(data))
+
+    }, [])
+
+    useEffect(() => {
+        getCovidData(country)
+    }, [getCovidData, country])
+
     return (
-        <div>
-            Teste
-        </div>
+        <ContainerStyled>
+            <div className='mb-2'>
+
+
+            </div>
+
+
+        </ContainerStyled>
     )
 }
 
