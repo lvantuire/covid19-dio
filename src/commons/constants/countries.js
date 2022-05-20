85385a2bc4b79ed7b74ff079f90c36da75bd2446
lvantuire <lvantuire@gmail.com>
2022-05-20 11:37:00 -0300

    inclusão dos dados Mundiais

--- src/commons/constants/countries.js
+++ src/commons/constants/countries.js
@@ -3,8 +3,14 @@ import ChinaFlag from '../../assets/images/china.png'
 import GermanyFlag from '../../assets/images/germany.png'
 import ItalyFlag from '../../assets/images/italy.png'
 import UsaFlag from '../../assets/images/usa.png'
-
+import WorldFlag from '../../assets/images/Mundo.png'
 const COUNTRIES = [
+  {
+    label: 'Mundo',
+    value: 'World',
+    flag: WorldFlag
+  },
+
   {
     label: 'Alemanha',
     value: 'germany',
